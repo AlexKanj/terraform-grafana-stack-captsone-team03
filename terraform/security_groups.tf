263827a89466f0000e9b50e4f263a78161d2de51
AlexKanj <alexkanj567@gmail.com>
2026-07-24 14:43:50 -0400

    added files for aws

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -12,6 +12,14 @@ resource "aws_security_group" "frontend" {
   }
 
   ingress {
+    description = "Team 03 Operations Portal"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "Grafana"
     from_port   = 3000
     to_port     = 3000
